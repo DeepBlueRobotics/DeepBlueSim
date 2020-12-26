f5b7b7e782324db0465ee8ff5eeff1e7dc2ee055
Dean Brettle <dean@brettle.com>
2020-12-25 16:24:58 -0800

    Remove the dependency on Mockito.
    This increases readability and reduces the size of the fat jar created
    by the Shadow gradle plugin from 5MB to 0.5MB.

--- src/main/java/code/lib/sim/SimRegisterer.java
+++ src/main/java/code/lib/sim/SimRegisterer.java
@@ -1,7 +1,6 @@
 package code.lib.sim;
 
-import java.util.function.Function;
-
+import java.util.concurrent.atomic.AtomicReference;
 import org.team199.wpiws.EncoderSim;
 import org.team199.wpiws.PWMSim;
 import org.team199.wpiws.ScopedObject;
@@ -10,8 +9,6 @@ import org.team199.wpiws.UniqueArrayList;
 import org.team199.wpiws.interfaces.BiLongCallback;
 import org.team199.wpiws.interfaces.BooleanCallback;
 import org.team199.wpiws.interfaces.SimDeviceCallback;
-
-import code.lib.Future;
 
 // Performs automatic registration of callbacks detecting both the initalization of new devices as well as data callbacks for devices such as Motors, Gyros, etc.
 // This allows us to automatically link these devices to Webots, reducing the amount of code we would have to change from a standard robot project
@@ -24,41 +21,36 @@ public class SimRegisterer {
         // Register Initalized Callbacks for Misc Devices
         SimDeviceSim.registerDeviceCreatedCallback("", MISC_DEVICE_CALLBACK, true);
         // Register Initalized Callbacks for PWM Devices
-        registerCallback(BooleanCallback.class, callback -> PWMSim.registerStaticInitializedCallback((name, isInitialized) -> {
+        PWMSim.registerStaticInitializedCallback((name, isInitialized) -> {
             if(isInitialized) {
                 callback("PWM", name, 0);
             }
-        }, true));
+        }, true);
         // Register Initalized Callbacks for Encoder Devices
-        registerCallback(BooleanCallback.class, callbackU -> EncoderSim.registerStaticInitializedCallback((name, isInitialized) -> {
+        EncoderSim.registerStaticInitializedCallback((name, isInitialized) -> {
             if(isInitialized) {
                 //When an Encoder is initalized, wait for it's channels to become available
-                registerCallback(BiLongCallback.class, callback -> new EncoderSim(name).registerChannelInitializedCallback((nameU, channelAL, channelB) -> {
+                AtomicReference<ScopedObject<BiLongCallback>> callbackRef = new AtomicReference<>();
+                callbackRef.set(new EncoderSim(name).registerChannelInitializedCallback((nameU, channelAL, channelB) -> {
                     int channelA = (int)channelAL;
                     PWMSim pwmSim = new PWMSim(channelA/2);
                     //Then wait for the associated PWM motor to be initalized
-                    registerCallback(BooleanCallback.class, callback2 -> pwmSim.registerInitializedCallback((nameU2, isInitialized2) -> {
+                    AtomicReference<ScopedObject<BooleanCallback>> callbackRef2 = new AtomicReference<>();
+                    callbackRef2.set(pwmSim.registerInitializedCallback((nameU2, isInitialized2) -> {
                         if(isInitialized2) {
                             //Then sync the values of that motor's encoder to the sim device
                             new MockedSparkEncoder("PWM_" + (channelA/2), channelA/2);
-                            callback2.close();
+                            callbackRef2.get().close();
                         }
                     }, true));
-                    callback.close();
+                    callbackRef.get().close();
                 }, true));
             }
-        }, true));
+        }, true);
     }
 
     // Initalize SimRegisterer. This method exists to ensure that the static block is called
     public static void init() {}
-
-    // Register callbacks for known devices on Non-Can ports
-    @SuppressWarnings("all")
-    private static <T> void registerCallback(Class<T> callbackType, Function<ScopedObject<T>, ScopedObject<T>> callbackRegisterFunc) {
-        Future<ScopedObject<T>> callback = new Future(ScopedObject.class);
-        callback.set(callbackRegisterFunc.apply(callback.get()));
-    }
 
     // Callback methods which place new devices in a processing queue which is processed every robot period
     // The WPILib callbacks are notified as part of the device creation. This process ensures that the devices complete their setup process
