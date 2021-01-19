2aadaa1bfba05666ca702978a9e22bd6aa277b16
Dean Brettle <dean@brettle.com>
2021-01-19 12:01:44 -0800

    Put Webots.zip at top level of plugin jar file.

--- plugin/src/main/groovy/org/team199/deepbluesim/gradle/DeepBlueSimPlugin.groovy
+++ plugin/src/main/groovy/org/team199/deepbluesim/gradle/DeepBlueSimPlugin.groovy
@@ -24,10 +24,11 @@ class DeepBlueSimPlugin implements Plugin<Project> {
                 void connect() throws IOException {
                     }
                 }
-                // def resourceURLString = getClass().getProtectionDomain().getCodeSource().getLocation().toExternalForm();
-                // System.out.println("resourceURLString = " + resourceURLString);
+                def resourceURLString = getClass().getProtectionDomain().getCodeSource().getLocation().toExternalForm();
+                System.out.println("resourceURLString = " + resourceURLString);
+                def resourceStream = DeepBlueSimPlugin.class.getResourceAsStream("/Webots.zip")
                 // def resourceStream = DeepBlueSimPlugin.class.getResourceAsStream(resourceURLString+"/org/team199/deepbluesim/gradle/Webots.zip")
-                def resourceStream = DeepBlueSimPlugin.class.getResourceAsStream("Webots.zip")
+                // def resourceStream = DeepBlueSimPlugin.classLoader.getResourceAsStream("/org/team199/deepbluesim/gradle/Webots.zip")
                 if (resourceStream == null) throw new RuntimeException("resourceStream is null")
                 def dbsDir = new File(project.buildDir, "tmp/deepbluesim")
                 dbsDir.mkdirs()
