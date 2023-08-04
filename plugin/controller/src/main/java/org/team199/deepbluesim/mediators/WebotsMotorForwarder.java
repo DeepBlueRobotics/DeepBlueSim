479ca38a41e2e0a746edc9e00d8f231207fe5209
CoolSpy3 <coolspythree@gmail.com>
2023-08-04 01:21:20 -0700

    remove patched workaround

--- plugin/controller/src/main/java/org/team199/deepbluesim/mediators/WebotsMotorForwarder.java
+++ plugin/controller/src/main/java/org/team199/deepbluesim/mediators/WebotsMotorForwarder.java
@@ -1,22 +1,17 @@
 package org.team199.deepbluesim.mediators;
 
-import com.cyberbotics.webots.controller.Motor;
-import com.cyberbotics.webots.controller.Node;
-import com.cyberbotics.webots.controller.Robot;
-import com.cyberbotics.webots.controller.Supervisor;
-
-import org.team199.deepbluesim.Simulation;
 import org.team199.wpiws.interfaces.DoubleCallback;
 import org.team199.wpiws.interfaces.StringCallback;
+
+import com.cyberbotics.webots.controller.Motor;
+import com.cyberbotics.webots.controller.Robot;
 
 /**
  * Forwards motor calls from WPILib motor controllers to Webots
  */
-public class WebotsMotorForwarder implements DoubleCallback, Runnable, StringCallback {
+public class WebotsMotorForwarder implements DoubleCallback, StringCallback {
 
-    private double currentOutput, pos, timer;
     private Motor motor;
-    private Node jointParameters;
 
     /**
      * Creates a new WebotsMotorForwarder
@@ -25,14 +20,6 @@ public class WebotsMotorForwarder implements DoubleCallback, Runnable, StringCal
      */
     public WebotsMotorForwarder(Robot robot, String motorName) {
         motor = robot.getMotor(motorName);
-        currentOutput = 0;
-        // Make sure that the motor can rotate any number of times
-        if(motor != null) {
-            motor.setPosition(Double.POSITIVE_INFINITY);
-            motor.setVelocity(0);
-            jointParameters = Supervisor.getSupervisorInstance().getFromDevice(motor).getParentNode();
-            Simulation.registerPeriodicMethod(this);
-        }
     }
 
     @Override
@@ -45,24 +32,7 @@ public class WebotsMotorForwarder implements DoubleCallback, Runnable, StringCal
 
     @Override
     public void callback(String name, double value) {
-        currentOutput = value;
-    }
-
-    @Override
-    public void run() {
-        if(timer == 0) {
-            timer = System.currentTimeMillis();
-            return;
-        }
-
-        double velocity = motor.getMaxVelocity() * currentOutput;
-        if(motor.getPositionSensor().getName().contains("CANCoder")) velocity *= motor.getMultiplier();
-        pos += velocity * (System.currentTimeMillis() - timer) / 1000;
-
-        if(motor.getPositionSensor().getName().contains("CANCoder")) jointParameters.setJointPosition(pos, 1);
-        else motor.setVelocity(velocity);
-
-        timer = System.currentTimeMillis();
+        motor.setVelocity(motor.getMaxVelocity() * value);
     }
 
 }
