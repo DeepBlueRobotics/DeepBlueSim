3ce638b1890bb31143e699ff70acc6322752848f
Dean Brettle <dean@brettle.com>
2024-05-06 20:49:48 -0700

    Keep robot and sim in sync during simulation.
    This works but still needs be cleaned up.

--- example/src/systemTest/java/frc/robot/SystemTestRobot.java
+++ example/src/systemTest/java/frc/robot/SystemTestRobot.java
@@ -7,6 +7,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import org.opencv.aruco.DetectorParameters;
+
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.hal.HALValue;
 import edu.wpi.first.hal.SimDevice;
@@ -15,6 +17,7 @@ import edu.wpi.first.hal.simulation.SimValueCallback;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
@@ -22,6 +25,8 @@ import edu.wpi.first.wpilibj.simulation.SimDeviceSim;
 import edu.wpi.first.wpilibj.simulation.SimHooks;
 
 public class SystemTestRobot extends Robot {
+
+    protected static final double MAX_TIME_DIFF_SECS = 0.0;
 
     public static void main(String... args) {
         RobotBase.startRobot(SystemTestRobot::new);
@@ -47,39 +52,105 @@ public class SystemTestRobot extends Robot {
     }
 
     SimDevice webotsSupervisor = null;
+    SimDevice timeSynchronizer = null;
     SimDouble positionX = null, positionY = null, positionZ = null;
-    SimDouble simTimeSec = null;
+    SimDouble simTimeSecSim = null;
+    SimDouble robotTimeSecSim = null;
+    CompletableFuture<Boolean> isDoneFuture = new CompletableFuture<Boolean>();
 
     @Override
     public void simulationInit() {
         webotsSupervisor = SimDevice.create("WebotsSupervisor");
-        SimDouble simStartMs = webotsSupervisor.createDouble("simStartMs", SimDevice.Direction.kInput, 0.0);
-        SimDouble robotStartMs = webotsSupervisor.createDouble("robotStartMs", SimDevice.Direction.kOutput, 0.0);
-        simTimeSec = webotsSupervisor.createDouble("simTimeSec", SimDevice.Direction.kInput, -1.0);
+        timeSynchronizer = SimDevice.create("TimeSynchronizer");
+        // SimDouble simStartMs = webotsSupervisor.createDouble("simStartMs", SimDevice.Direction.kInput, 0.0);
+        // SimDouble robotStartMs = webotsSupervisor.createDouble("robotStartMs", SimDevice.Direction.kOutput, 0.0);
+        simTimeSecSim = timeSynchronizer.createDouble("simTimeSec", SimDevice.Direction.kInput, -1.0);
+        robotTimeSecSim = timeSynchronizer.createDouble("robotTimeSec", SimDevice.Direction.kOutput, -1.0);
         positionX = webotsSupervisor.createDouble("self.position.x", SimDevice.Direction.kInput, 0.0);
         positionY = webotsSupervisor.createDouble("self.position.y", SimDevice.Direction.kInput, 0.0);
         positionZ = webotsSupervisor.createDouble("self.position.z", SimDevice.Direction.kInput, 0.0);
-        SimDeviceSim webotsSupervisorSim = new SimDeviceSim("WebotsSupervisor");
+        // SimDeviceSim webotsSupervisorSim = new SimDeviceSim("WebotsSupervisor");
+        SimDeviceSim timeSynchronizerSim = new SimDeviceSim("TimeSynchronizer");
 
         // Wait for the Webots supervisor to be ready
-        final var future = new CompletableFuture<Boolean>();
-        try (var callback = webotsSupervisorSim.registerValueChangedCallback(simStartMs, new SimValueCallback() {
-            @Override
-            public void callback(String name, int handle, int direction, HALValue value) {
-                if (value.getDouble() > 0.0) {
-                    System.out.println("WebotsSupervisor is ready.");
-                    System.out.println("Telling WebotsSupervisor that we're ready");
-                    robotStartMs.set(System.currentTimeMillis());
-                    future.complete(true);
+        final var isReadyFuture = new CompletableFuture<Boolean>();
+        // try (var callback = webotsSupervisorSim.registerValueChangedCallback(simStartMs, new SimValueCallback() {
+        //     @Override
+        //     public void callback(String name, int handle, int direction, HALValue value) {
+        //         if (value.getDouble() > 0.0) {
+        //             System.out.println("WebotsSupervisor is ready.");
+        //             System.out.println("Telling WebotsSupervisor that we're ready");
+        //             robotStartMs.set(System.currentTimeMillis());
+        //             isReadyFuture.complete(true);
+        //         }
+        //     }
+        // }, true))
+        {
+            // System.out.println("Telling WebotsSupervisor that we're ready");
+            // robotStartMs.set(System.currentTimeMillis());
+            // if (simStartMs.get() > 0.0) {
+            //     System.out.println("WebotsSupervisor is ready");
+            //     isReadyFuture.complete(true);
+            // }
+
+            final Notifier pauser = new Notifier(SimHooks::pauseTiming);
+
+            timeSynchronizerSim.registerValueChangedCallback(simTimeSecSim, new SimValueCallback() {
+                @Override
+                public synchronized void callback(String name, int handle, int direction, HALValue value) {
+                    double simTimeSec = value.getDouble();
+                    System.out.println("simTime valueChangedCallback called with " + simTimeSec);
+                    double robotTimeSec = Timer.getFPGATimestamp();
+                    System.out.println("robotTimeSec = " + robotTimeSec);
+                    double deltaSecs = simTimeSec + MAX_TIME_DIFF_SECS - robotTimeSec;
+                    if (simTimeSec == -1.0) {
+                        return;
+                    }
+                    if (robotTimeSecSim.get() == -2.0) {
+                        // Waiting for sim to start
+                        if (simTimeSec == -2.0) {
+                            // Sim just started so restart robot timing
+
+                            if (!isReadyFuture.isDone()) {
+                                System.out.println("Calling isReadyFuture.complete(true)");
+                                isReadyFuture.complete(true);
+                            }
+                        }
+                    } else if (deltaSecs >= 0.0) {
+                        // Tell the sim what the new robot time is. Strictly speaking it won't be that time
+                        // until stepTiming() returns but this allows the sim to run in parrallel with the robot code.
+                        // It's less deterministic but faster.
+                        // System.out.println(String.format("calling robotTimeSec.set(%g)", Timer.getFPGATimestamp() + deltaSecs));
+                        if (isDoneFuture.getNow(false).booleanValue()) {
+                            // Call endCompetition() to end the test and report success.
+                            // NOTE: throwing an exception will end the test and report failure.
+                            System.out.println("Calling endCompetition()");
+                            endCompetition();
+                            System.out.println("endCompetition() returned");
+                        } else {
+                            robotTimeSec += deltaSecs;
+                            // Let robot code run for deltaSecs. We use a Notifier instead of SimHooks.stepTiming() because 
+                            // using SimHooks.stepTiming() causes accesses to sim data to block.
+                            System.out.println("Calling pauser.stop()");
+                            // Make any awaiting pauser run doesn't run before our new one.
+                            pauser.stop();
+                            System.out.println("Calling pauser.startSingle()");
+                            pauser.startSingle(deltaSecs);
+                            System.out.println("Calling resumeTiming()");
+                            SimHooks.resumeTiming();
+                            System.out.println("resumeTiming() returned");    
+                            // System.out.println(String.format("Calling stepTiming(%g)", deltaSecs));
+                            // SimHooks.stepTiming(deltaSecs);
+                            // System.out.println("stepTiming() returned");    
+                        }
+                    }
+                    if (robotTimeSec != robotTimeSecSim.get()) {
+                        System.out.println("Calling robotTimeSecSim() with " + robotTimeSec);
+                        robotTimeSecSim.set(robotTimeSec);
+                    }
                 }
-            }
-        }, true)) {
-            System.out.println("Telling WebotsSupervisor that we're ready");
-            robotStartMs.set(System.currentTimeMillis());
-            if (simStartMs.get() > 0.0) {
-                System.out.println("WebotsSupervisor is ready");
-                future.complete(true);
-            }
+            }, true);
+
             // Reset the clock. Without this, *Periodic calls that should have 
             // occurred while we waited, will be considered behind schedule and
             // will all happen at once.
@@ -88,15 +159,11 @@ public class SystemTestRobot extends Robot {
             // Pause the clock so that we can step it in sync with the simulator
             SimHooks.pauseTiming();
 
-            webotsSupervisorSim.registerValueChangedCallback(simTimeSec, new SimValueCallback() {
-                @Override
-                public void callback(String name, int handle, int direction, HALValue value) {
-                    double deltaSecs = value.getDouble() - Timer.getFPGATimestamp();
-                    if (deltaSecs > 0.0) {
-                        SimHooks.stepTimingAsync(deltaSecs);
-                    }
-                }
-            }, true);
+            // Tell sim to start
+            robotTimeSecSim.set(-2.0);
+            
+            // Tell sim that we're ready
+            // robotTimeSecSim.set(Timer.getFPGATimestamp());
 
             // Wait up to 15 minutes for Webots to respond. On GitHub's MacOS Continuous
             // Integration servers, it can take over 8 minutes for Webots to start.
@@ -107,7 +174,12 @@ public class SystemTestRobot extends Robot {
                 try {
                     long elapsedTime = System.currentTimeMillis() - startedWaitingTimeMs;
                     long remainingTime = 900000 - elapsedTime;
-                    if(remainingTime > 0) isReady = future.get(remainingTime, TimeUnit.MILLISECONDS);
+                    System.out.println("Waiting for isReadyFuture");
+                    if(remainingTime > 0) {
+                        System.out.println("Waiting for isReadyFuture");
+                        isReady = isReadyFuture.get(remainingTime, TimeUnit.MILLISECONDS);
+                        System.out.println("isReadyFuture.get() returned " + isReady);
+                    }
                     else isReady = true;
                 } catch (TimeoutException ex) {
                     System.err.println("Waiting for WebotsSupervisor to be ready. Please open example/Webots/worlds/DBSExample.wbt in Webots.");
@@ -115,33 +187,50 @@ public class SystemTestRobot extends Robot {
                     throw new RuntimeException("Error while waiting for WebotsSupervisor to be ready", e);
                 }
             }
+            System.out.println("Done waiting");
             assertTrue("Webots ready in time", isReady);
+            System.out.println("Done asserting");
         }
 
-        System.out.println("WebotsSupervisor is ready. Enabling in autonomous.");
+        System.out.println("WebotsSupervisor is ready. Enabling in autonomous."); System.out.flush();
+
         // Simulate starting autonomous
         DriverStationSim.setAutonomous(true);
         DriverStationSim.setEnabled(true);
         DriverStationSim.notifyNewData();
 
         super.simulationInit();
+        System.out.println("Time at end of simulationInit="+Timer.getFPGATimestamp());
     }
 
     @Override
     public void simulationPeriodic() {
+        System.out.println("Time at start of simulationPeriodic="+Timer.getFPGATimestamp());
+        // if (Timer.getFPGATimestamp() > simTimeSecSim.get()) {
+        //     SimHooks.pauseTiming();
+        //     System.out.println(String.format("calling robotTimeSec.set(%g)", Timer.getFPGATimestamp()));
+        //     robotTimeSec.set(Timer.getFPGATimestamp());
+        // }
+
         super.simulationPeriodic();
 
+        System.out.println("In simulationPeriodic()");
+        System.out.println("Calling positionX.get()");
+        System.out.println("self.position.x =" + positionX.get());
         // The motors are on for 2 secs. We wait an extra second to give the robot time to stop.
         if (Timer.getFPGATimestamp() > 3.0) {
             // Simulate disabling the robot
+            System.out.println("Disabling robot");
             DriverStationSim.setEnabled(false);
             DriverStationSim.notifyNewData();
+            System.out.println("Done disabling");
 
             Vector<N3> expectedPos = new Vector<>(N3.instance);
             expectedPos.set(0, 0, -2.6);
             expectedPos.set(1, 0, 0.0);
             expectedPos.set(2, 0, 0.0);
 
+            System.out.println("Calling positionX.get()");
             System.out.println("self.position.x =" + positionX.get());
             System.out.println("self.position.y =" + positionY.get());
             System.out.println("self.position.z =" + positionZ.get());
@@ -154,11 +243,11 @@ public class SystemTestRobot extends Robot {
             var diff = new Vector<N3>(expectedPos.minus(actualPos));
             var distance = Math.sqrt(diff.elementTimes(diff).elementSum());
 
+            System.out.println("Asserting robot near targe position");
             assertEquals("Robot close to target position", 0.0, distance, 1.0);
 
-            // Call endCompetition() to end the test and report success.
-            // NOTE: throwing an exception will end the test and report failure.
-            endCompetition();
+            System.out.println("Calling isDoneFuture.complete(true)");
+            isDoneFuture.complete(true);
         }
     }
 }
--- plugin/controller/src/main/java/DeepBlueSim.java
+++ plugin/controller/src/main/java/DeepBlueSim.java
@@ -1,6 +1,7 @@
 import java.lang.Thread.UncaughtExceptionHandler;
 import java.net.URISyntaxException;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.CompletableFuture;
 
 import com.cyberbotics.webots.controller.Node;
@@ -21,10 +22,13 @@ import org.team199.deepbluesim.Simulation;
 // the name of the jar.
 public class DeepBlueSim {
 
-    private static final ConcurrentLinkedDeque<Runnable> queuedMessages = new ConcurrentLinkedDeque<>();
+    private static final BlockingDeque<Runnable> queuedMessages = new LinkedBlockingDeque<>();
+
+    protected static final double MAX_TIME_DIFF_SECS = 0.0;
 
     @SuppressWarnings("unused")
-    private static ScopedObject<Pair<String, StringCallback>> callbackStore = null;
+    private static ScopedObject<Pair<String, StringCallback>> robotStartMsCallbackStore = null;
+    private static ScopedObject<Pair<String, StringCallback>> robotTimeSecCallbackStore = null;
     private static RunningObject<WebSocketClient> wsConnection = null;
 
     public static void main(String[] args) {
@@ -51,11 +55,16 @@ public class DeepBlueSim {
             System.err.println("The robot does not have supervisor=true. This is required to detect devices.");
             System.exit(1);
         }
+        // Get the basic timestep to use for calls to robot.step()
+        final int basicTimeStep = (int)Math.round(robot.getBasicTimeStep());
+
         Simulation.init(robot, robot.getBasicTimeStep());
 
         // Use a SimDeviceSim to coordinate with robot code
-        final CompletableFuture<Boolean> future = new CompletableFuture<Boolean>();
+        final CompletableFuture<Boolean> isDoneFuture = new CompletableFuture<Boolean>();
         final SimDeviceSim webotsSupervisorSim = new SimDeviceSim("WebotsSupervisor");
+        final SimDeviceSim timeSynchronizerSim = new SimDeviceSim("TimeSynchronizer");
+
         {
             // Regular report the simulated robot's position
             Simulation.registerPeriodicMethod(new Runnable() {
@@ -68,38 +77,77 @@ public class DeepBlueSim {
                 }
             });
 
+            // Whenever the robot time changes, step the simulation until just past that time
+            robotTimeSecCallbackStore = timeSynchronizerSim.registerValueChangedCallback("robotTimeSec", new StringCallback() {
+                @Override
+                public synchronized void callback(String name, String value) {
+                    System.out.println("In robotTimeSec callback with value = " + value); System.out.flush();
+            
+                    if (value != null) {
+                        double robotTimeSec = Double.parseDouble(value);
+
+                        if (robotTimeSec == -2.0) {
+                            System.out.println("Reloading world");
+                            robot.worldReload();
+                            return;
+                        }
+                        // Keep stepping the simulation forward until
+                        // the sim time is more than MAX_TIME_DIFF_SECS ahead of the robot time
+                        // or the simulation ends.
+                        while(true) {
+                            double simTimeSec = robot.getTime();
+                            if (simTimeSec > robotTimeSec + MAX_TIME_DIFF_SECS) {
+                                break;
+                            }
+                            System.out.println("Calling robot.step(basicTimeStep) in robotTimeSec callback"); System.out.flush();
+                            boolean isDone = (robot.step(basicTimeStep) == -1);
+                            System.out.println("robot.step(basicTimeStep) returned in robotTimeSec callback"); System.out.flush();
+                            timeSynchronizerSim.set("simTimeSec", robot.getTime());
+                            if (isDone) {
+                                isDoneFuture.complete(true);
+                                break;
+                            }
+                            Simulation.runPeriodicMethods();
+                        }
+                    }
+                    System.out.println("exiting robotTimeSec callback"); System.out.flush();
+                }
+            }, true);
+
+
             // If the robot code starts after us, we expect it to tell us it's ready, and we respond
             // that we're ready.
-            callbackStore = webotsSupervisorSim.registerValueChangedCallback("robotStartMs", new StringCallback() {
-                @Override
-                public void callback(String name, String value) {
-                    if (value != null) {
-                        System.out.println("Telling the robot we're ready");
-                        webotsSupervisorSim.set("simStartMs", System.currentTimeMillis());
-                        future.complete(true);
-                    }
-            }
-            }, true);
+            // robotStartMsCallbackStore = webotsSupervisorSim.registerValueChangedCallback("robotStartMs", new StringCallback() {
+            //     @Override
+            //     public void callback(String name, String value) {
+            //         if (value != null) {
+            //             System.out.println("Telling the robot we're ready");
+            //             webotsSupervisorSim.set("simStartMs", System.currentTimeMillis());
+            //             isReadyFuture.complete(true);
+            //         }
+            //     }
+            // }, true);
 
             // If the robot code starts before we us, then it might have already tried to tell
             // us it was ready and we would have missed it. So, we tell it we're ready when we
             // connect to it.
             ConnectionProcessor.addOpenListener(() -> {
-                System.out.println("Telling the robot we're ready");
-                webotsSupervisorSim.set("simStartMs", System.currentTimeMillis());
-                future.complete(true);
+                System.out.println("Telling the robot we're ready"); System.out.flush();
+                // webotsSupervisorSim.set("simStartMs", System.currentTimeMillis());
+                timeSynchronizerSim.set("simTimeSec", -2.0);
             });
         }
-
-        // Get the basic timestamp to use for calls to robot.step()
-        int basicTimeStep = (int)Math.round(robot.getBasicTimeStep());
-
-        // Wait until one timestep has completed to ensure that the Webots simulator is
+        // Wait until startup has completed to ensure that the Webots simulator is
         // not still starting up.
-        if (robot.step(basicTimeStep) == -1) {
-            throw new RuntimeException("Couldn't even do one timestep!");
+        System.out.println(String.format("Calling robot.step(%d)...", 0));
+        System.out.flush();
+        if (robot.step(0) == -1) {
+            throw new RuntimeException("Couldn't even start up!");
         }
+        System.out.println("Calling connectDevices()...");
+        System.out.flush();
         SimRegisterer.connectDevices();
+
 
         // Connect to the robot code
         try {
@@ -114,26 +162,27 @@ public class DeepBlueSim {
             return;
         }
 
+        System.out.println("connectHALSim() returned");
+
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try {
                 wsConnection.object.closeBlocking();
             } catch(InterruptedException e) {}
         }));
 
-        try {
-            future.get();
-        } catch (Exception ex) {
-            throw new RuntimeException("Exception while waiting for robot to be ready");
-        }
+        // Tell robot we're ready
+        System.out.println("telling robot we're ready. robot.getTime()=" + robot.getTime());
+        timeSynchronizerSim.set("simTimeSec", -2.0);
 
-        System.out.println("Starting simulation");
+        System.out.println("Waiting for simulation to finish...");
         System.out.flush();
-        do {
-            webotsSupervisorSim.set("simTimeSec", robot.getTime());
-            queuedMessages.forEach(Runnable::run);
-            queuedMessages.clear();
-            Simulation.runPeriodicMethods();
-        } while(robot.step(basicTimeStep) != -1);
+        try {
+            while (isDoneFuture.getNow(false).booleanValue() == false) {
+                queuedMessages.takeFirst().run();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Exception while waiting for simulation to be done");
+        }
 
         System.out.println("Shutting down DeepBlueSim...");
         System.out.flush();
