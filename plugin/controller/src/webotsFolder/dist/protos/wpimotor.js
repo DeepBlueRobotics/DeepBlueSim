1b47ca0c440f830a0232bc21e036905b2937ec1c
CoolSpy3 <coolspythree@gmail.com>
2024-06-30 23:30:06 -0700

    more formatting

--- plugin/controller/src/webotsFolder/dist/protos/wpimotor.js
+++ plugin/controller/src/webotsFolder/dist/protos/wpimotor.js
@@ -5,14 +5,14 @@
             gearing = gearing / fields.outputRadiusMeters.value;
         }
         return `
-        name "${["DBSim_Motor", fields.controllerType.value, fields.port.value, gearing, fields.inverted.value, fields.nominalVoltageVolts.value, fields.stallTorqueNewtonMeters.value, fields.stallCurrentAmps.value, fields.freeCurrentAmps.value, fields.freeSpeedRPM.value].join('_')}"
-        maxVelocity ${(2 * Math.PI / 60) * fields.freeSpeedRPM.value / gearing}
-        ${fields.outputRadiusMeters ? "maxForce" : "maxTorque"} ${fields.stallTorqueNewtonMeters.value * gearing}
-        # The documentation about the multiplier field is unclear as to how it applies differently to different fields / functions.
-        # I think it's best just to implement it ourselves for now.
-        # multiplier ${1 / gearing}
-        ${(fields.sound.value !== "default") ? "soundUrl IS sound" : ""}
-`;
+            name "${["DBSim_Motor", fields.controllerType.value, fields.port.value, gearing, fields.inverted.value, fields.nominalVoltageVolts.value, fields.stallTorqueNewtonMeters.value, fields.stallCurrentAmps.value, fields.freeCurrentAmps.value, fields.freeSpeedRPM.value].join('_')}"
+            maxVelocity ${(2 * Math.PI / 60) * fields.freeSpeedRPM.value / gearing}
+            ${fields.outputRadiusMeters ? "maxForce" : "maxTorque"} ${fields.stallTorqueNewtonMeters.value * gearing}
+            # The documentation about the multiplier field is unclear as to how it applies differently to different fields / functions.
+            # I think it's best just to implement it ourselves for now.
+            # multiplier ${1 / gearing}
+            ${(fields.sound.value !== "default") ? "soundUrl IS sound" : ""}
+        `;
     },
     getDef: function (motorType) {
         const specs = {
@@ -115,16 +115,16 @@
         };
         let spec = specs[motorType];
         return `
-        controllerType IS controllerType
-        port IS ${spec.port}
-        gearing IS gearing
-        inverted IS inverted
-        nominalVoltageVolts ${spec.nominalVoltageVolts}
-        stallTorqueNewtonMeters ${spec.stallTorqueNewtonMeters}
-        stallCurrentAmps ${spec.stallCurrentAmps}
-        freeCurrentAmps ${spec.freeCurrentAmps}
-        freeSpeedRPM ${spec.freeSpeedRPM}
-        sound IS sound
-    `;
+            controllerType IS controllerType
+            port IS ${spec.port}
+            gearing IS gearing
+            inverted IS inverted
+            nominalVoltageVolts ${spec.nominalVoltageVolts}
+            stallTorqueNewtonMeters ${spec.stallTorqueNewtonMeters}
+            stallCurrentAmps ${spec.stallCurrentAmps}
+            freeCurrentAmps ${spec.freeCurrentAmps}
+            freeSpeedRPM ${spec.freeSpeedRPM}
+            sound IS sound
+        `;
     },
 })
